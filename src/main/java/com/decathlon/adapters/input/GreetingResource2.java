4d8f076a293211c372455725add0a71f212d0f08
Prasanth Kutcharlapati <prasanth.k@cloverbaytechnologies.com>
2024-04-22 12:45:59 +0530

    get a

---
+++ src/main/java/com/decathlon/adapters/input/GreetingResource2.java
@@ -0,0 +1,17 @@
+package com.decathlon.adapters.input;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hi2")
+public class GreetingResource2 {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String> hello() {
+        return Uni.createFrom().item("This is a Quarkus Reactive application");
+    }
+}
---
+++ src/test/java/com/decathlon/GreetingResource2IT.java
@@ -0,0 +1,8 @@
+package com.decathlon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResource2IT extends GreetingResource2Test {
+    // Execute the same tests but in packaged mode.
+}
